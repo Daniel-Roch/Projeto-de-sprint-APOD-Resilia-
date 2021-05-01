f14378202a56800b228c866191fc9f7418c45a32
Daniel-Roch <daniel_roch4@hotmail.com>
2021-05-01 19:30:38 -0300

    Melhorando o Site, e colocando vídeo

--- main.js
+++ main.js
@@ -6,24 +6,27 @@ $('#btn-enviar').click(function(event){
     $.ajax({
         'url': `https://api.nasa.gov/planetary/apod?api_key=wvOmQJqdKJGo9dgb6TgpKXPGsSdffxLcov4TY0wu&date=${valueData}`,
         'success': function(result){
-            console.log(result)
-            $('body').css('background-image' , `url(${result.url})`)
-            $('#reposta-data').html(`<h1 class="titulo-result">${result.title}</h1>
-            <br>
-            <p class="p-resultado">${result.explanation}</p>
-            <br>
-            <a href="${result.url}">Click Aqui para visualizar a imagem melhor.</a>`)
-            //colocarNaPag(result) // Peguei um função pra jogar em outro lugar
+            addPag(result)
         }
     })
 $('.invisivel').css('display', 'none')
 $('.invisivel-quadro').css('display', 'flex')
 })
 
-
-/*function colocarNaPag(result){
-    $('#reposta-data').html(`<img class="img-result" src=${result.hdurl}>`)
-    $('.img-result').css('width', '50vh')
-    $('.img-result').css('height', '50vh')
-}*/
-//se puxar a data : 28/09/2019 aparecer uma frase em homenagem a bia
+function addPag(result){
+            $('body').css('background-image' , `url(${result.url})`)
+            $('#reposta-data').html(`<h1 class="titulo-result">${result.title}</h1>
+            <br>
+            <h2 class="p-resultado">${result.explanation}</h2>
+            <br>
+            <a href="${result.url}">Click Aqui para visualizar a imagem melhor.</a>
+            <br>
+            `)
+            var youtube = result.url.substr(12,7) //Saber se na url tem escrito youtube.
+            if(youtube == 'youtube'){ // se houve, ele irá adicionar os elementos a baixo.
+            $('#reposta-data').append(`<iframe class="video-assistir" width="560" height="315" src="${result.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+            $('body').css('background-image' , `url('./img/astronauta.jpg')`)
+            $('#section-reposta').css('display', 'flex')
+            $('#section-reposta').css('flex-direction', 'column') //flex-direction: column;
+            }
+}
